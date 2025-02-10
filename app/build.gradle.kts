55e6660b1d51c153054851b6e48a095eaa61d9f9
ryohamag <Ryojune21@gmail.com>
2025-02-10 20:51:19 +0900

    Retrofit,Gsonの導入

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -56,4 +56,10 @@ dependencies {
     androidTestImplementation(libs.androidx.ui.test.junit4)
     debugImplementation(libs.androidx.ui.tooling)
     debugImplementation(libs.androidx.ui.test.manifest)
+
+    //Retrofit
+    implementation(libs.retrofit)
+
+    //Gson
+    implementation(libs.converter.gson)
 }
